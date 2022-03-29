a9ffde58080f4a9b87bc5d76f1707aefb5aaa605
gajen0981 <gajen.sunthara@gmail.com>
2022-03-29 11:12:03 -0400

    Updated to dev portal + basic patient list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { AppHeader } from "@commure/components-core";
-import { CommureSmartApp } from "@commure/components-data";
+import { AppHeader, PatientCard } from "@commure/components-core";
+import { CommureSmartApp, FhirDataQuery } from "@commure/components-data";
+import { Bundle, Patient } from "@commure/fhir-types/r4/types";
 import SMARTClient from "@commure/smart-core";
 import React from "react";
 import "./App.css";
@@ -10,9 +11,35 @@ const smartClient = new SMARTClient(smartConfig);
 function App() {
   return (
     <CommureSmartApp client={smartClient}>
-      <AppHeader appName="My First Commure App" fixedToTop />
+      <AppHeader appName="Commure | Low-Code | SDKs" fixedToTop />
       <div className="hello-world-container">
-        <p>Hello world!</p>
+
+        <FhirDataQuery queryString="Patient">
+          {({ data, loading }) => {
+            if (loading) {
+              return "Loading...";
+            }
+            if (!data) {
+              return "Error loading data!";
+            }
+            const entries = (data as Bundle).entry!;
+            if (!entries) {
+              return "Data is empty! (Forgot to import?)";
+            }
+            /* Rendering each of the patients below here */
+            const patients: Patient[] = entries.map(
+              (value) => value.resource as Patient
+            );
+            return (
+              <div>
+                {patients.map((patient, index) => (
+                  <PatientCard key={index} resource={patient} />
+                ))}
+              </div>
+            );
+          }}
+        </FhirDataQuery>
+
       </div>
     </CommureSmartApp>
   );
