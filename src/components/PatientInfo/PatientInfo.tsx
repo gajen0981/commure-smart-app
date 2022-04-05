eeaaf3dd35b6735afe909b8ed896a64bd6e09700
gajen0981 <gajen.sunthara@gmail.com>
2022-04-05 14:51:32 -0400

    PatientCard component

--- src/components/PatientInfo/PatientInfo.tsx
+++ src/components/PatientInfo/PatientInfo.tsx
@@ -5,6 +5,8 @@ import Condition from "../Condition/Condition";
 import Fishbone from "../Fishbone/Fishbone";
 import Medications from "../Medications/Medications";
 import Vitals from "../Vitals/Vitals";
+import { PatientCard } from "@commure/components-core";
+import { FhirDataQuery } from "@commure/components-data";
 
 interface Props {
   patientId: string;
@@ -16,17 +18,30 @@ const PatientInfo: React.FC<Props> = ({ patientId }) => {
     baseClass,
     patientId
   };
+
   return (
     <div className={baseClass}>
-        <Medications {...baseProps} />
-        <div className="column">
-            <Condition {...baseProps} />
-            <Allergies {...baseProps} />
-        </div>
-        <Vitals {...baseProps} />
-        <h1>Labs</h1>
-        <Fishbone {...baseProps} fishboneType="BMP" />
-        <Fishbone {...baseProps} fishboneType="CBC" />
+      <div>
+        <br/>
+        <FhirDataQuery queryString={"Patient/" + patientId}>
+          {({ data }) => (
+            <PatientCard resource={data} />
+          )}
+        </FhirDataQuery>
+      </div>
+
+      <Medications {...baseProps} />
+
+      <div className="column">
+          <Condition {...baseProps} />
+          <Allergies {...baseProps} />
+      </div>
+
+      <Vitals {...baseProps} />
+
+      <h1>Labs</h1>
+      <Fishbone {...baseProps} fishboneType="BMP" />
+      <Fishbone {...baseProps} fishboneType="CBC" />
     </div>
   );
 };
